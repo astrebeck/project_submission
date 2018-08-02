b1c0faa08f2503541045420a1c98400cbce0773c
astrebeck <alexstrebeck@gmail.com>
2018-08-02 08:30:51 -0600

    Upload of image txt files

---
+++ src/process.py
@@ -0,0 +1,32 @@
+import glob, os
+
+#used to create test.txt and train.txt
+#
+
+# Current directory
+current_dir = 'C:/Users/Smartsrc-alex/Documents/ML_class/project_submission/Images/Out/'
+
+# Directory where the data will reside, relative to 'darknet.exe'
+path_data = 'data/obj/'
+
+# Percentage of images to be used for the test set
+percentage_test = 10
+
+# Create and/or truncate train.txt and test.txt
+file_train = open('train.txt', 'w')  
+file_test = open('test.txt', 'w')
+
+# Populate train.txt and test.txt
+counter = 1  
+index_test = round(100 / percentage_test)
+
+for pathAndFilename in glob.glob(os.path.join(current_dir, "*.JPEG")):  
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+    if counter == index_test:
+        counter = 1
+        file_test.write(path_data + title + '.JPEG' + "\n")
+
+    else:
+        file_train.write(path_data + title + '.JPEG' + "\n")
+        counter = counter + 1
